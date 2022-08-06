6f63e02f8b8d5a1b624b59b18212787282bb4433
june22scgjavab <june22scgjavab@gmail.com>
2022-08-06 13:43:23 +0530

    13:43

---
+++ springdataaoploggerdemo/validator/Validator.java
@@ -0,0 +1,60 @@
+package com.example.validator;
+
+import java.time.LocalDate;
+
+import com.example.dto.CustomerDTO;
+import com.example.exception.CustomerException;
+
+
+public class Validator {
+
+	
+	public static void validate(CustomerDTO customerDTO) throws CustomerException
+	{
+		String message=null;
+		if(!validateCustomerId(customerDTO.getCustomerId()))
+		{
+			message="Validator.INVALID_ID";
+		}
+		if(!validateCustomerId(customerDTO.getEmailId()))
+		{
+			message="Validator.INVALID_EMAIL";
+		}
+		if(!validateCustomerName(customerDTO.getName()))
+		{
+			message="Validator.INVALID_NAME";
+		}
+		if(!validateCustomerName(customerDTO.getName()))
+		{
+			message="Validator.INVALID_EMAIL";
+		}
+		if(!validateCustomerName(customerDTO.getDateOfBirth()))
+		{
+			message="Validator.INVALID_DOB";
+		}
+	 if(message!=null) {
+		 throw new CustomerException(message);
+	 }
+	
+	}
+
+	private static boolean validateCustomerName(LocalDate dateOfBirth) {
+		
+		return dateOfBirth.isBefore(LocalDate.now()) ? true : false;
+	}
+
+	private static boolean validateCustomerName(String name) {
+		
+		return name.matches("^([A-Z][a-z]+)(\\s[A-Z][a-z]+){1,2}$") ? true : false;
+				
+	}
+
+	private static boolean validateCustomerId(String emailId) {
+		return emailId.matches("^[a-z]+@infy\\.com$") ? true : false;
+	}
+
+	private static boolean validateCustomerId(int customerId) {
+		
+		return customerId<=0?false : true;
+	}
+}
